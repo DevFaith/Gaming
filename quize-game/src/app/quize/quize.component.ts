6097d5b9238de9519d69a63a3ce55808e71ed60d
DevFaith <waitherafaith@outlook.com>
2025-03-06 23:22:22 +0300

    Implement timer functionality in QuizComponent to auto-advance questions and reset timer for each question

--- quize-game/src/app/quize/quize.component.ts
+++ quize-game/src/app/quize/quize.component.ts
@@ -28,14 +28,43 @@ export class QuizComponent {
   score = 0;
   showResult = false;
 
-  selectAnswer(option: string) {
-    if (option === this.questions[this.currentQuestionIndex].answer) {
-      this.score++;
-    }
+  timer: number = 30; // 30 seconds per question
+  interval: any;
+
+  constructor() {
+    this.startTimer(); // Start the timer when the quiz loads
+  }
+
+  startTimer() {
+    this.interval = setInterval(() => {
+      if (this.timer > 0) {
+        this.timer--;
+      } else {
+        this.nextQuestion(); // Auto move to next question on timeout
+      }
+    }, 1000);
+  }
+
+  resetTimer() {
+    clearInterval(this.interval);
+    this.timer = 30;
+    this.startTimer(); // Restart the timer for the next question
+  }
+
+  nextQuestion() {
+    this.resetTimer();
     this.currentQuestionIndex++;
 
     if (this.currentQuestionIndex === this.questions.length) {
       this.showResult = true;
+      clearInterval(this.interval); // Stop the timer when quiz ends
     }
+  }
+
+  selectAnswer(option: string) {
+    if (option === this.questions[this.currentQuestionIndex].answer) {
+      this.score++;
+    }
+    this.nextQuestion();
   }
 }
