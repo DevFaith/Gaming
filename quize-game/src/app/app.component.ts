0128edab634dd1586f643b668f2c247c3dacf3e3
DevFaith <waitherafaith@outlook.com>
2025-03-06 20:18:30 +0300

    Add QuizComponent to AppComponent imports

--- quize-game/src/app/app.component.ts
+++ quize-game/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {QuizComponent } from './quize/quize.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, QuizComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
